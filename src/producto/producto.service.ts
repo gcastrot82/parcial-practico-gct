6a58152ea2122d7f6384d71159a04d07d6b3ded7
gcastrot82 <gcastrot82@hotmail.com>
2023-11-19 07:37:38 -0500

    Update: Crea la logica para el recurso producto

--- src/producto/producto.service.ts
+++ src/producto/producto.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { ProductoEntity } from './producto.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import {
+  BusinessLogicException,
+  BusinessError,
+} from 'src/shared/errors/business-errors';
 
 @Injectable()
 export class ProductoService {
@@ -9,4 +13,53 @@ export class ProductoService {
     @InjectRepository(ProductoEntity)
     private readonly productoRepository: Repository<ProductoEntity>,
   ) {}
+  async findAll(): Promise<ProductoEntity[]> {
+    return await this.productoRepository.find({ relations: ['tiendas'] });
+  }
+
+  async findOne(id: string): Promise<ProductoEntity> {
+    const producto: ProductoEntity = await this.productoRepository.findOne({
+      where: { id },
+      relations: ['productos'],
+    });
+    if (!producto)
+      throw new BusinessLogicException(
+        'El producto ingresado no fue encontrado',
+        BusinessError.NOT_FOUND,
+      );
+
+    return producto;
+  }
+  async create(producto: ProductoEntity): Promise<ProductoEntity> {
+    return await this.productoRepository.save(producto);
+  }
+
+  async update(id: string, producto: ProductoEntity): Promise<ProductoEntity> {
+    const persistedProducto: ProductoEntity =
+      await this.productoRepository.findOne({
+        where: { id },
+      });
+    if (!persistedProducto)
+      throw new BusinessLogicException(
+        'El producto ingresado no fue encontrado',
+        BusinessError.NOT_FOUND,
+      );
+
+    return await this.productoRepository.save({
+      ...persistedProducto,
+      ...producto,
+    });
+  }
+  async delete(id: string) {
+    const producto: ProductoEntity = await this.productoRepository.findOne({
+      where: { id },
+    });
+    if (!producto)
+      throw new BusinessLogicException(
+        'El producto ingresado no fue encontrado',
+        BusinessError.NOT_FOUND,
+      );
+
+    await this.productoRepository.remove(producto);
+  }
 }
